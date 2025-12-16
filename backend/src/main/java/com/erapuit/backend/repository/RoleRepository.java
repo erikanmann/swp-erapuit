828383e456c4bf3ae845600c6882c11072327855
erikanmann <erikanmann@gmail.com>
2025-12-17 00:48:13 +0200

    provides database access methods for the Role and User entity, #64

---
+++ backend/src/main/java/com/erapuit/backend/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.erapuit.backend.repository;
+
+import com.erapuit.backend.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Optional<Role> findByName(Role.RoleType name);
+}
