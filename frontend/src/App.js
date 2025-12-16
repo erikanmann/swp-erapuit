bdc84ffefd1eccbf72d7d7f804739ba11291b8a6
erikanmann <erikanmann@gmail.com>
2025-12-17 00:33:04 +0200

    added authentication, /login and protectedRoute + new pages #65

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,102 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RegisterDeliveryPage from './pages/RegisterDeliveryPage';
-import './styles/main.css';
+import LoginPage from './pages/LoginPage';
+import UserManagementPage from './pages/UserManagementPage';
+import UserProfilePage from './pages/UserProfilePage';
+import ProtectedRoute from './components/ProtectedRoute';
 import WarehouseDashboard from './pages/WarehouseDashboard';
 import ProductionUsagePage from './pages/ProductionUsagePage';
 import OutboundShippingPage from "./pages/OutboundShippingPage";
 import DeliveryDetailPage from "./pages/DeliveryDetailPage";
+import { tokenStorage } from './api/authApi';
+import { UserProvider } from './context/UserContext';
+
+function AppContent() {
+    return (
+        <Routes>
+            {/* Public Routes */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<Navigate to="/login" replace />} />
+
+            {/* Protected Routes */}
+            <Route
+                path="/home"
+                element={
+                    <ProtectedRoute>
+                        <HomePage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/register-delivery"
+                element={
+                    <ProtectedRoute>
+                        <RegisterDeliveryPage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/deliveries/:id"
+                element={
+                    <ProtectedRoute>
+                        <DeliveryDetailPage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/warehouse-dashboard"
+                element={
+                    <ProtectedRoute>
+                        <WarehouseDashboard />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/production-usage"
+                element={
+                    <ProtectedRoute>
+                        <ProductionUsagePage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/outbound-shipping"
+                element={
+                    <ProtectedRoute>
+                        <OutboundShippingPage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/user-management"
+                element={
+                    <ProtectedRoute>
+                        <UserManagementPage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/profile"
+                element={
+                    <ProtectedRoute>
+                        <UserProfilePage />
+                    </ProtectedRoute>
+                }
+            />
+        </Routes>
+    );
+}
 
 function App() {
     return (
         <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/home" element={<HomePage />} />
-                    <Route path="/register-delivery" element={<RegisterDeliveryPage />} />
-                    <Route path="/deliveries/:id" element={<DeliveryDetailPage />} />
-                    <Route path="/warehouse" element={<WarehouseDashboard />} />
-                    <Route path="/production-usage" element={<ProductionUsagePage />} />
-                    <Route path="/outbound-shipping" element={<OutboundShippingPage />} />
-                </Routes>
-            </div>
+            <UserProvider>
+                <div className="App">
+                    <AppContent />
+                </div>
+            </UserProvider>
         </Router>
     );
 }
